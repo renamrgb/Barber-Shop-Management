9e8ce6775b055a31376dc981a59b45469d5d5ea2
Rafael Oliveira <rafael8paulo@gmail.com>
2023-04-14 17:17:45 -0300

    Start Scheduling

---
+++ backend/bsm/src/main/java/com/rpx/bsm/entities/Scheduling.java
@@ -0,0 +1,14 @@
+package com.rpx.bsm.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class Scheduling {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Auto incremento
+    private Long Id;
+
+
+
+}
